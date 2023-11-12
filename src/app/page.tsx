a275c7ae0ae30ff70b7f87eb52d06a56ad13b67f
jiwon <semanticist0@gmail.com>
2023-11-12 14:01:02 +0900

    🚧 splash screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,15 @@ import MainContent from "layouts/main_cotent";
 import MainTitle from "layouts/main_titlebar";
 import MainWrapper from "layouts/main_wrapper";
 import ThemeProvder from "layouts/theme_provider";
-import SplashHandler from "components/splash";
 
 const Home: FC = () => {
   return (
-    <SplashHandler>
-      <ThemeProvder>
-        <MainWrapper>
-          <MainTitle />
-          <MainContent />
-        </MainWrapper>
-      </ThemeProvder>
-    </SplashHandler>
+    <ThemeProvder>
+      <MainWrapper>
+        <MainTitle />
+        <MainContent />
+      </MainWrapper>
+    </ThemeProvder>
   );
 };
 
